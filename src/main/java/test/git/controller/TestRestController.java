048a3ffcb85f8600ea843276f31ac25a47b02328
Home_PC <truq1990@gmail.com>
2024-05-28 12:48:31 +0400

    feat: create restController

---
+++ src/main/java/test/git/controller/TestRestController.java
@@ -0,0 +1,15 @@
+package test.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestRestController {
+
+  @GetMapping
+  public String test() {
+    return "test".toUpperCase();
+  }
+}
